fbfea081f78958e56d893fe21a1ecf6857189bd1
pengliheng <pengliheng111@gmail.com>
2018-07-30 03:15:43 +0800

    add pop improvePop algorithms

--- index.js
+++ index.js
@@ -1,75 +1,33 @@
-(function(window){
-	class Vue {
-		constructor(opt){
-			this.opt = opt
-			// 将所有属性便利到Vue实例上面
-			for(let i in opt.data()){
-				// 劫持数据
-				this.bind({
-					key: i,
-					val: this.opt.data()[i]
-				})
-			}
-			for(let i in opt.methods){
-				this.opt[i] = this.opt.methods[i];
-			}
-			// 创建
-			this.opt.created && this.opt.created()
+import Vue from './vue.js'
+import {
+	pop,
+	improvePop,
+} from './sort.js';
+const sweap = (a,b) => [a,b] = [ b, a];
+import 'babel-polyfill';
 
-			// 挂载
-			this.opt.mounted && this.opt.mounted()
 
-			// 初始化html数据，将{{}}以及:替换成真实数据
-			this.dataToHTML();
 
-			// 初始化html数据，将{{}}以及:替换成真实数据
-			this.init();
-			this.direct = {};
+new Vue({
+	el:document.querySelector('#root'),
+	data(){
+		return {
+			time:0,
+			arr: [4,3,2,1,8,7,6,5]
 		}
-		dataToHTML () {
-			// 完整实现替换
-			
-			// 3.预埋hash值
-			let root = document.querySelector('#root').innerHTML;
-			for(let i in this.opt.data()){
-				const root = document.querySelector('#root').outerHTML;
-				this[i] = this.opt.data()[i];
-				let reg = new RegExp(`<.*>(?=.*\n.*\{\{${i}\}\})`,'gmi');
-				console.log(
-					root.match(reg)
-				);
-				// root = root.replace(reg,'$1')
-			}
-			// 1.{{}}  替换
-			// 2.:attr 替换
-			for(let i in this.opt.data()){
-				this[i] = this.opt.data()[i];
-				let reg = new RegExp(`\{\{${i}\}\}`,'gmi');
-				root = root.replace(reg,this.opt.data()[i])
-			}
-			document.querySelector('#root').innerHTML = root
-		}
-		init () {
-			document.querySelector('input').addEventListener('keydown',e => {
-				const val = e.target.value;
-				[...e.target.attributes].filter(e=>/:/.test(e.name)).forEach((el,i)=>{
-					this.opt[e.target.getAttribute(el.name)] = val;
-				});
-			})
-		}
-		bind({key,val}){
-			Object.defineProperty(this.opt,key,{
-				get () {
-					console.log('u have been get '+key)
-					return val
-				},
-				set (v) {
-					console.log('需要去同步HTML模板 '+key, v)
-					// that.dataToHTML();
-					val = v;
-				},
-			})
-		}
+	},
+	async mounted(){
+		// 交换策略  ,某个数字位于当前数字的 位置
+		const result = improvePop(this.arr,400 , (j)=>{
+			this.time++;
+			[this.arr[j], this.arr[j+1]] = [this.arr[j+1], this.arr[j]]
+			this.arr = [...this.arr]
+			return this.arr;
+		})
+		console.log(await result);
+	},
+	created(){
+		window.app = this;
+		console.log('i am created');
 	}
-	window.Vue = Vue;
-})(window)
+})
---
+++ vue.js
@@ -0,0 +1,144 @@
+
+class Vue {
+	constructor(opt){
+		// _内部数据
+		// $外部数据
+		this.opt = opt;
+		this._html = ()=>document.querySelector('#root').innerHTML;
+		this.temphtml = document.querySelector('#root').innerHTML;
+		this.root = document.querySelector('#root');
+		this.opt._data = this.opt.data();
+		this.dep = {};
+		// 创建
+		this.opt.created && this.opt.created()
+		// 将所有属性便利到Vue实例上面
+		for(let i in this.opt._data){
+			// 劫持数据
+			this.bind({
+				key: i,
+				val: this.opt._data[i]
+			})
+		}
+		for(let i in this.opt.methods){
+			this[i] = this.opt.methods[i];
+		}
+		// 初始化html数据，将{{}}以及:替换成真实数据
+		for(let i in this.opt._data){
+			this.initDataToHTML(i);
+		}
+		// 初始化html数据，将{{}}以及:替换成真实数据
+		this.init();
+		// 挂载
+		this.opt.mounted && this.opt.mounted()
+	}
+	getHash(len){
+		return Number(~~(Math.random()*len)).toString(16);
+	}
+	// to set every dom an tip, whilch can let me to find this.dom
+	setDomTip(dom){
+		const hash = this.getHash(999999);
+		dom.setAttribute(`data-v-0${hash}0`,'');
+		return `data-v-0${hash}0`;
+	}
+	// 初始化的时候采用整体替换的策略
+	initDataToHTML (i) {
+		// 全部模块都替换一次
+		let root = this.root;
+		// to replace v-for loop function
+		[
+			...root.querySelectorAll('div'),
+			...root.querySelectorAll('span')
+		].filter(dom=>{
+			
+			if(dom.getAttribute('v-for')){
+				let reg = new RegExp(`\\w+\\sin\\s${i}`,'gmi');
+				if(dom.getAttribute('v-for').match(reg)){
+					return true;
+				}
+			}
+		}).forEach(dom=>{
+			let variable = dom.getAttribute('v-for').split(' ')[0];
+			// 将for循环替换
+			this.opt[i].forEach((e,i,arr) =>{
+				let span  = dom.cloneNode();
+				span.removeAttribute('v-for');
+				this.dep[`this.arr.${i}`] = this.setDomTip(span);
+				let time = this.opt.time;
+				[...span.attributes].filter(attr=> /^:/.test(attr.localName)).forEach(attr => {
+					attr.ownerElement.setAttribute(attr.localName.replace(/^\:/,''),eval(attr.value))
+					// span.removeAttribute(attr.localName);
+				})
+				let content = dom.innerHTML;
+				// let reg = new RegExp(`\\{\\{[\s\S]+\\}\\}`,'gmi');
+				// content = content.replace(reg,'eval($1)')
+				
+				span.innerHTML = i+1;
+				dom.parentElement.insertBefore(span,dom)
+			})
+			dom.remove()
+		})
+
+		// let html = this._html();
+		// for(let i in this._data){
+		// 	// to replace {{variable}}
+		// 	let reg = new RegExp(`\{\{(${i})\}\}`,'gmi');
+		// 	html = html.replace(reg,this[i]);
+		// }
+		// this.root.innerHTML = html
+	}
+	// 修改数据的时候整体替换的方法不可取啊...
+	modifyDataToHTML (key,isModify) {
+		// 考虑数组长度变化
+		if(typeof this.opt[key] === 'object'){
+			this.opt[key].forEach((e,i,arr)=>{
+				const style = this.root.querySelector(`[${this.dep[`this.${key}.${i}`]}]`).getAttribute(':style')
+				let time = this.opt.time;
+				root.querySelector(`[${this.dep[`this.${key}.${i}`]}]`).style.transform = `translate(${(arr.indexOf(i+1))*120}%,${time*20}%)`
+				// root.querySelector(`[${this.dep[`this.${key}.${i}`]}]`).innerHTML = i+1;
+			})
+
+
+			// this.opt[i].forEach((e,i) =>{
+			// 	let time = this.opt.time;
+			// 	[...span.attributes].filter(attr=> /^:/.test(attr.localName)).forEach(attr => {
+			// 		attr.ownerElement.setAttribute(attr.localName.replace(/^\:/,''),eval(attr.value))
+			// 		span.removeAttribute(attr.localName);
+			// 	})
+			// 	let content = dom.innerHTML;
+			// 	let reg = new RegExp(`\{\{(${variable})\}\}`,'gmi');
+			// 	content = content.replace(reg,i+1)
+			// 	span.innerHTML = content;
+			// 	dom.parentElement.insertBefore(span,dom)
+			// })
+		}
+	}
+	init () {
+		document.querySelector('input') && document.querySelector('input').addEventListener('keyup',e => {
+			const val = e.target.value;
+			[...e.target.attributes].filter(e=>/:/.test(e.name)).forEach((el,i)=>{
+				this[e.target.getAttribute(el.name)] = val;
+			});
+		})
+	}
+	bind({key,val}){
+		var that = this;
+		Object.defineProperty(this.opt,key,{
+			get () {
+				return val
+			},
+			set (v) {
+				if(this[key].length === v.lenth){
+					// length not modify
+					val = v;
+					that.modifyDataToHTML(key,false);
+				}else{
+					// modify
+					val = v;
+					that.modifyDataToHTML(key,true);
+				}
+			},
+		})
+	}
+}
+
+export default Vue;
