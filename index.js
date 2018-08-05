bcf3a47101a3eca28ad476b4738d9b5ad7b699c9
pengliheng <pengliheng111@gmail.com>
2018-08-06 03:49:19 +0800

    add circledoublelinklist structures

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import {
 	CricleDoubleLinkList,
 } from './lib/structures';
 import 'babel-polyfill';
+import { timingSafeEqual } from 'crypto';
 
 
 new Vue({
@@ -18,17 +19,66 @@ new Vue({
 	data(){
 		return {
 			time: 0,
-			arr: [8,7,6,5,4,3,2,1]
+			arr: [1,2,3,4,5]
 		}
 	},
 	mounted() {
 		const criDbLinkList = new CricleDoubleLinkList();
-		criDbLinkList.append(222);
-		criDbLinkList.append(123);
-		criDbLinkList.append('是否');
-		criDbLinkList.append('不是');
+		this.arr.forEach(arr=>{
+			criDbLinkList.append(arr);
+		})
+		// 将第一个元素推出,压入数组最后;
+		console.log(this.arr);
 		
-		window.db = dbLinkList;
+		// setInterval(() => {
+		// 	criDbLinkList.append(
+		// 		criDbLinkList.unshift().val
+		// 	);
+		// 	this.arr = criDbLinkList.outputArr();
+		// 	console.log(this.arr);
+		// }, 1000);
+		document.querySelector('#root span:nth-child(3)').addEventListener('touchstart',(e)=>{
+			let clientX = e.changedTouches[0].clientX;
+			let times = 0;
+			const proxyFunc = (e)=>{
+				const _clientX = e.changedTouches[0].clientX;
+				if(times === Math.floor((_clientX - clientX)/100)){
+					console.log('not diff');
+					return 
+				}else{
+					console.log('diff');
+					times = Math.floor((_clientX - clientX)/100);
+				}
+				if(times>0){
+					clientX = _clientX;
+					criDbLinkList.shift(
+						criDbLinkList.pop().val
+					);
+				}else{
+					clientX = _clientX;
+					criDbLinkList.append(
+						criDbLinkList.unshift().val
+					);
+				}
+				this.arr = criDbLinkList.outputArr();
+
+				console.log(times);
+				
+			}
+			e.target.addEventListener('touchmove',proxyFunc)
+			e.target.addEventListener('touchend',(e)=>{
+				e.target.removeEventListener('touchmove',proxyFunc);
+			})
+		})
+		
+		
+		// 将最后一个元素推出,压入数组头部;
+		// criDbLinkList.shift(
+		// 	criDbLinkList.pop().val
+		// )
+		
+		
+		window.db = criDbLinkList;
 	},
 	created(){
 		window.app = this;
